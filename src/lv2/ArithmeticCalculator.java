bf35bb89b2ffeee084d9f6edd0828e9875fb289b
PARKJUNYONG <treeoark1@gmail.com>
2026-06-30 21:46:02 +0900

    lv2 step10

--- src/lv2/ArithmeticCalculator.java
+++ src/lv2/ArithmeticCalculator.java
@@ -5,39 +5,28 @@ public class ArithmeticCalculator extends Calculator {
     SubtractOperator sub;
     MultiplyOperator mlt;
     DivideOperator div;
+    ModOperator mod;
 
     public ArithmeticCalculator(){
         this.add = new AddOperator();
         this.sub = new SubtractOperator();
         this.mlt = new MultiplyOperator();
         this.div = new DivideOperator();
+        this.mod = new ModOperator();
     }
 
     public int calculate(int num1, int num2, char operator) {
         if(num1 < 0 || num2 < 0){
             throw new IllegalArgumentException("양의 정수(0 포함)만 입력할 수 있습니다");
         }
-        int result;
-        switch (operator){
-            case '+':
-                result = add.operate(num1,num2);
-                break;
-            case '-':
-                result = sub.operate(num1, num2);
-                break;
-            case '*':
-                result = mlt.operate(num1, num2);
-                break;
-            case '/':
-                if (num2 == 0) {
-                    throw new IllegalArgumentException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                }
-                result = div.operate(num1, num2);
-                break;
-
-            default:
-                throw new IllegalArgumentException("잘못된 연산자를 입력했습니다. (+, -, *, /)");
-        }
+        int result = switch (operator){
+            case '+' -> add.operate(num1, num2);
+            case '-' -> sub.operate(num1, num2);
+            case '*' -> mlt.operate(num1, num2);
+            case '/' -> div.operate(num1, num2);
+            case '%' -> mod.operate(num1, num2);
+            default -> throw new IllegalArgumentException("잘못된 연산자를 입력했습니다. (+, -, *, /, %)");
+        };
         return result;
     }
 }
--- src/lv2/ModOperator.java
+++ src/lv2/ModOperator.java
@@ -1,10 +1,10 @@
 package lv2;
 
-public class DivideOperator {
+public class ModOperator {
     public int operate(int num1, int num2) {
         if (num2 == 0) {
             throw new IllegalArgumentException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
         }
-        return num1 / num2;
+        return num1 % num2;
     }
 }
