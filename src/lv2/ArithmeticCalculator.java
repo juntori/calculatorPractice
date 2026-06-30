76f0870667c07239a61643fa0f15b0de98388ccc
PARKJUNYONG <treeoark1@gmail.com>
2026-06-30 21:33:16 +0900

    lv2 step9

--- src/lv2/ArithmeticCalculator.java
+++ src/lv2/ArithmeticCalculator.java
@@ -1,6 +1,17 @@
 package lv2;
 
 public class ArithmeticCalculator extends Calculator {
+    AddOperator add;
+    SubtractOperator sub;
+    MultiplyOperator mlt;
+    DivideOperator div;
+
+    public ArithmeticCalculator(){
+        this.add = new AddOperator();
+        this.sub = new SubtractOperator();
+        this.mlt = new MultiplyOperator();
+        this.div = new DivideOperator();
+    }
 
     public int calculate(int num1, int num2, char operator) {
         if(num1 < 0 || num2 < 0){
@@ -9,19 +20,19 @@ public class ArithmeticCalculator extends Calculator {
         int result;
         switch (operator){
             case '+':
-                result = num1 + num2;
+                result = add.operate(num1,num2);
                 break;
             case '-':
-                result = num1 - num2;
+                result = sub.operate(num1, num2);
                 break;
             case '*':
-                result = num1 * num2;
+                result = mlt.operate(num1, num2);
                 break;
             case '/':
                 if (num2 == 0) {
                     throw new IllegalArgumentException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                 }
-                result = num1 / num2;
+                result = div.operate(num1, num2);
                 break;
 
             default:
---
+++ src/lv2/MultiplyOperator.java
@@ -0,0 +1,7 @@
+package lv2;
+
+public class MultiplyOperator {
+    public int operate(int num1, int num2) {
+        return num1 * num2;
+    }
+}
